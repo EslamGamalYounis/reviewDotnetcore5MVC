4d650cdd0d89f139d50fee2847b1257f40a7daea
Islam Gamal <eslam.gamal.younis@gmail.com>
2022-02-18 15:59:27 +0200

    add-scaffolded-identity

---
+++ CourseMvcNet5Part1StartProject/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CourseMvcNet5Part1StartProject.Areas.Identity.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
+
